1dd4d47836d0f028243963deb07091111dfddb83
JaishAgrawal <jaishaggarwal8400@gmail.com>
2026-06-01 19:59:44 +0530

    Day 01 assignment 4 problems

---
+++ Day-1/question1.c
@@ -0,0 +1,14 @@
+//wap to calculate sum of first N natural numbers.
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+        {
+        sum+=i; //sum=sum+i
+}
+    printf("Sum of first %d natural numbers is: %d", n, sum);
+    return 0;
+}
